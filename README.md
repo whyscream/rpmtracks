4ebe5420e512a1a623f448268159ea96e08e74b9
Tom Hendrikx <tom@whyscream.net>
2025-09-09 22:24:12 +0200

    Initial version of the rpmtracks script
    Scrapes the wiki page containing all data,
    parses the content and prints the data on stdout.

---
+++ README.md
@@ -0,0 +1,24 @@
+# RPM Tracks
+
+Create a list of Les Mills RPM tracks. to find the tracks you like. Use it to search through tracks from earlier releases and suggest tracks to your instructor for a mix training.
+
+## Installation 
+
+1. Clone the repository:
+
+    ```shell
+    git clone <url>
+    cd rpmtracks
+    ```
+
+2. Install the required packages:
+
+    ```shell
+    uv sync --frozen
+    ```
+
+3. Run the application:
+
+    ```shell
+    uv run main.py
+    ```
---
+++ main.py
@@ -0,0 +1,86 @@
+import re
+
+import httpx
+from bs4 import BeautifulSoup
+
+SOURCE_URL = "https://seesaawiki.jp/tracklist/d/RPM"
+
+
+def parse_track(row):
+    cols = row.find_all("td")
+    if cols[0].attrs.get("colspan") or cols[0].attrs.get("rowspan"):
+        # This is a image column, discard it
+        del cols[0]
+
+    if len(cols) == 6:
+        track_id = cols[0].get_text(strip=True)
+        track_name = cols[1].get_text(strip=True)
+        track_author = cols[2].get_text(strip=True)
+        track_cover_artist = cols[3].get_text(strip=True)
+        track_duration = cols[4].get_text(strip=True)
+        track_type = cols[5].get_text(strip=True)
+    elif len(cols) == 5:
+        track_id = cols[0].get_text(strip=True)
+        track_name = cols[1].get_text(strip=True)
+        track_author = cols[2].get_text(strip=True)
+        track_cover_artist = None
+        track_duration = cols[3].get_text(strip=True)
+        track_type = cols[4].get_text(strip=True)
+    else:
+        raise ValueError(f"Unexpected number of columns: {len(cols)}")
+
+    # remove duplicate durations
+    if match := re.match(r"(\d+:\d{2})", track_duration):
+        track_duration = match.group(1)
+
+    # Extract RPM release from track_id
+    release = None
+    track_number = None
+    # track id RPM or BB + all numerics is a normal track
+    if match := re.match(r"(?:RPM|BB)(?P<release>\d+)(?P<track_number>\d{2})", track_id):
+        release = match.group("release")
+        track_number = match.group("track_number")
+    # track id RPM + numeric + random string is a bonus track of sorts
+    elif match := re.match(r"(?:RPM|BB)(?P<release>\d+)(?P<track_number>.*)", track_id):
+        release = match.group("release")
+        track_number = match.group("track_number")
+    elif "RPMUN" in track_id:
+        # No idea what this is, skipping for now
+        return None
+
+    track = {
+        "id": track_id,
+        "release": release,
+        "track_number": track_number,
+        "name": track_name,
+        "author": track_author,
+        "cover_artist": track_cover_artist,
+        "duration": track_duration,
+        "type": track_type,
+    }
+    print(track)
+    return track
+
+
+def main():
+    # Request the URL
+    response = httpx.get(SOURCE_URL)
+    # Find the tracklist in the HTML source using beautifulsoup
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    tracklists = []
+    tracklists.append(soup.find("table", id="content_block_1"))
+    tracklists.append(soup.find("table", id="content_block_4"))
+    tracklists.append(soup.find("table", id="content_block_7"))
+    tracklists.append(soup.find("table", id="content_block_10"))
+    # Extract the track data from the tracklists
+    tracks = []
+    for tracklist in tracklists:
+        for row in tracklist.find_all("tr")[1:]:
+            track = parse_track(row)
+            tracks.append(track)
+
+    print(f"Total tracks: {len(tracks)}")
+
+if __name__ == "__main__":
+    main()
